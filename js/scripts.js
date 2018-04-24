7fcb846f02a0ba1758dbda88329d20a80e8e7c5e
Benard18 <benkaranja43@gmail.com>
2018-04-24 16:57:10 +0300

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
       var num1 = parseFloat(prompt("Enter your number:"));
       var num2 = parseFloat(prompt("Enter your other number:"));
-      var result = (num1 * num2);
+      var result = (num1 / num2);
       alert(result);
